c3f7668a3e856dab5542697be60fb4e5fff972ea
anton-ua <antonyua23@gmail.com>
2020-03-31 14:11:20 +0300

    Updates

---
+++ precache-manifest.9ec6224233c1e0a325564623658da506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f5d45db3f9a396d400ae537e0977b4",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "138b3755c7996399b929",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f4328095.chunk.css"
+  },
+  {
+    "revision": "77a37187bbd8bbcc2394",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.f7ebff9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.f7ebff9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138b3755c7996399b929",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d7e09289.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
